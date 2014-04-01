9b829ea65107a3dcebf95e122038c9c590ea25dc
Espuma <eduardmaxjonkman@gmail.com>
2014-04-01 16:34:59 +0200

    multiple
    fit in screen, grouping on prefix, sizing on contig size

--- nummer2.js
+++ nummer2.js
@@ -1,17 +1,9 @@
 var fileDisplay=document.getElementById('display');
+var graaf={"nodes":[],"links":[],"usedNodes":[]} //{"nodes":[{}],"links":[{},{},{}]}
 
 window.onload=function(){
 //load file; file recognition not yet implemented, below code is for formatting .dot files
-var bestand=document.getElementById('bestand');
-bestand.addEventListener('change',function(){
-laadBestand=bestand.files[0];
-var reader=new FileReader();
-reader.readAsText(laadBestand,"UTF-8");
-//store file in var
-reader.onload=function(event){
-var geladen=event.target.result;
-regels=geladen.split("\n");
-var graaf={"nodes":[],"links":[]} //{"nodes":[{}],"links":[{},{},{}]}
+
 var vindNode = function (id) {
         for (var i in graaf["nodes"]) {
 			if (graaf["nodes"][i]["id"] === id) {
@@ -20,49 +12,64 @@ var vindNode = function (id) {
 		};
 	}
 
+var groepen=[];
+var kleurGroep = function(id){
+	if (!(id.substr(0,3) in groepen)){groepen.push(id.substr(0,3))}
+	return groepen.indexOf(id.substr(0,3));
+};	
+	
+bestand.addEventListener('change',function(){
+	laadBestand=bestand.files[0];
+	var reader=new FileReader();
+	reader.readAsText(laadBestand,"UTF-8");
+	//store file in var
+reader.onload=function(event){
+	var geladen=event.target.result;
+	regels=geladen.split("\n");
+	
 for(i=0;i<regels.length;i++){//asqg parsing
 
 if (regels[i].split("\t")[0]==="VT"){//collect nodes
-n=regels[i].split("\t")[1]
-seq=regels[i].split("\t")[2]
-graaf.nodes[graaf.nodes.length]={"id":n,"sequence":seq}
-
-}//end if loop
+	var id=regels[i].split("\t")[1]
+	var seq=regels[i].split("\t")[2]
+	graaf.nodes[graaf.nodes.length]={"id":id,"sequence":seq,"group":kleurGroep(id)}
+}//end node if loop
 
 if(regels[i].split("\t")[0]==="ED"){//collect links and overlap information
-info=regels[i].split("\t")[1]
-s=info.split(" ")[0]
-t=info.split(" ")[1]
-sos=parseInt(info.split(" ")[2])
-sol=parseInt(info.split(" ")[3])
-tos=parseInt(info.split(" ")[5])
-tol=parseInt(info.split(" ")[6])
-revcomp=parseInt(info.split(" ")[8])
-graaf.links[graaf.links.length]={"source":vindNode(s),"target":vindNode(t),"sStart":sos,"sEnd":sol,"tStart":tos,"tEnd":tol,"revcomp":revcomp}//if revcomp=1, reverse one of the seqs to match them
-
-}//de naam van de contig wordt nu opgeslagen als source/target, niet de plaats in de lijst van nodes. findNode() moet nog gebruikt worden.
+	info=regels[i].split("\t")[1]
+	s=info.split(" ")[0]
+	t=info.split(" ")[1]
+	sos=parseInt(info.split(" ")[2])
+	sol=parseInt(info.split(" ")[3])
+	tos=parseInt(info.split(" ")[5])
+	tol=parseInt(info.split(" ")[6])
+	revcomp=parseInt(info.split(" ")[8])
+	graaf.links[graaf.links.length]={"source":vindNode(s),"target":vindNode(t),"sStart":sos,"sEnd":sol,"tStart":tos,"tEnd":tol,"revcomp":revcomp}//if revcomp=1, reverse one of the seqs to match them
+	if(!(s in graaf.usedNodes)){graaf.usedNodes.push({"id":s})};
+	if(!(t in graaf.usedNodes)){graaf.usedNodes.push({"id":t})};
+	
+}//end link if loop
 }//end asqg for loop
 //load graph with nodes and links
 window.graaf=graaf;
 	var maxLen=0;
-	console.log(maxLen);
 	for (i in graaf.nodes){if (graaf.nodes[i]["sequence"].length>maxLen){maxLen=graaf.nodes[i]["sequence"].length}};
-	console.log(maxLen);
-	var width = 6600,
-		height = 6400,
+	var w=6600,
+		h=6400,
 		zoomx=1,
 		zoomy=1;
 	
 	var color = d3.scale.category20();
 
 	var svg = d3.select("body").append("svg")
-		.attr("width", width)
-		.attr("height", height);
+		.attr("id", "graafsvg")
+		.attr("viewBox", "0 0 " + w + " " + h )
+		.attr("preserveAspectRatio", "xMidYMid meet")
 
 	var force = d3.layout.force()
 		.nodes(graaf.nodes)
 		.links(graaf.links)
-		.size([width, height])
+		.size([w, h])
 		.charge(-200)
 		.on("tick", tick)
 		.start();
@@ -76,8 +83,8 @@ window.graaf=graaf;
 	   .data(graaf.nodes)
 	 .enter().append("circle")
 	   .attr("class", "node")
-	   .attr("r", function(d){return 5*(d.source.sequence.length/maxLen)})
-	   .style("fill", function(d) { return color(d.group); })
+	   .attr("r", function(d){return 10*(d.sequence.length/maxLen)})
+	   .style("fill", function(d) { if(d.group===0){return "blue"}else{return "orange"}})
 	   .call(force.drag);
 
 	function tick() {
@@ -89,7 +96,7 @@ window.graaf=graaf;
 	  node.attr("cx", function(d) { return d.x; })
 		  .attr("cy", function(d) { return d.y; });
 	}
-	
+
 }//end reader.onload
 })//end bestand.addeventlistener
 };//end window.onload
